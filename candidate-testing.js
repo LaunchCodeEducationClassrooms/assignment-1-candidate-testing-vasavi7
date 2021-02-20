cb8d3d0f5c13b9643f6c42843d5b8f0c6a93020e
vasavi7 <vasavi7@outlook.com>
2021-02-20 21:56:30 +0000

    Code formatted, spaces removed.

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,8 +10,7 @@ let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters = 5 kilometers. ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
-let candidateAnswers = [];
-
+let candidateAnswers=[];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -20,7 +19,7 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  for (let i = 0; i < questions.length; i++) {
+  for(let i=0; i<questions.length; i++) {
     console.log(`${i + 1}) ${questions[i]}`);
     candidateAnswers[i] = input.question("Your Answer: ");
     console.log(`Correct Answer: ${correctAnswers[i]} \n`);
@@ -31,9 +30,9 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-  let correctAnswersCount = 0;
-  let grade = 0;
-  for (let j = 0; j < questions.length; j++) {
+  let correctAnswersCount=0;
+  let grade=0;
+  for(let j=0; j<questions.length; j++) {
     if (candidateAnswers[j].toLowerCase() === correctAnswers[j].toLowerCase()) {
       correctAnswersCount++;
     }
